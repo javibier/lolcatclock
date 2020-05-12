b3764108cd4358849009ee1d1e01f69a25335a8b
Javier Echevarria <javierechevarria21@yahoo.com>
2020-05-12 15:32:03 -0400

    update js lolcatbutton

--- script.js
+++ script.js
@@ -91,3 +91,35 @@ updateClock();
 var oneSecond = 1000;
 
 setInterval( updateClock, oneSecond);
+
+var lolcatButton = document.getElementById("lolcatButton");
+var lolcatImage = document.getElementById("lolcatImage");
+var image = "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/08/napTime.jpg";
+ 
+var showMeTheLolcat = function(){
+    lolcatImage.src = image;
+};
+ 
+lolcatButton.addEventListener("click", showMeTheLolcat);
+
+// set isPartyTime to false
+var isPartyTime = false;
+ 
+var partyEvent = function() {
+   
+   //check isPartyTime
+   if (isPartyTime === false){ 
+        // if isPartyTime is false, change it to true 
+        //so we know the button has been clicked
+        isPartyTime = true; 
+        // set time to partyTime so the lolCat clock 
+        //image and message update to the partyTime image and message
+        time = partyTime; 
+    } else { 
+        // if isPartyTime is true, change it to false to end the party
+        isPartyTime = false; 
+        // set time back to the current time
+        time = new Date().getHours(); 
+    }
+ 
+};
